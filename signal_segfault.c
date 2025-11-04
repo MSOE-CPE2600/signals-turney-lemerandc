6532a6d3008070f6167b2e57a644505d5fbb69d9
lemerandc <lemerandc@msoe.edu>
2025-11-04 11:03:17 -0600

    Part 3.1 of lab

--- signal_segfault.c
+++ signal_segfault.c
@@ -4,17 +4,25 @@
  */
 
 /**
- * Modified by:
+ * Modified by: Charlie Lemerand
  * 
- * Brief summary of modifications:
+ * Brief summary of modifications: created the function fault_signal to
+ * catch any seg faults
  */
 
 
 #include <stdio.h>
+#include <signal.h>
+
+void fault_signal(){
+    printf("A seg fault has occured\n");
+}
 
 int main (int argc, char* argv[]) {
     // Declare a null pointer
     int* i = NULL;
+
+    signal(SIGSEGV, fault_signal);
 
     // Dereference the null pointer
     printf("The value of i is: %d\n", *i);
